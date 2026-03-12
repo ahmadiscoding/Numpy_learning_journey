417db982a820fa742ff26c7a093c454388f68d2d
Ahmad <muhammadahmad1892@gmail.com>
2026-03-13 01:10:32 +0500

    indexing

---
+++ Day2_Indexing_Slicing/01_indexing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# -----------------------------
+# positive and negative Indexing 
+# -----------------------------
+
+arrays=np.array([10,20,40,50])
+print(arrays[3]) #positive
+print(arrays[-1]) #negative
