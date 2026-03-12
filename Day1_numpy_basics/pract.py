c54cfba6fcdcd3ffe454c2e78620d8891453dc6c
Ahmad <muhammadahmad1892@gmail.com>
2026-03-12 16:15:23 +0500

    some exercise

---
+++ Day1_numpy_basics/pract.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+# Create a 4 × 4 matrix of zeros.
+# Then print:
+# the matrix
+# its shape
+# its size
+arrays=np.zeros((4,4))
+# [[2,4,5,1], [6,7,2,8], [9,10,4,8], [5,1,4,0]]
+print(arrays)
+print(arrays.shape)
+print(arrays.ndim)
+print(arrays.size)
+
+
