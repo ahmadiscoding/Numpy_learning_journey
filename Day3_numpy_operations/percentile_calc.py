e300319757332d9bc2c6a06100442cf09c170c1d
Ahmad <muhammadahmad1892@gmail.com>
2026-03-16 03:25:21 +0500

    calculator

---
+++ Day3_numpy_operations/percentile_calc.py
@@ -0,0 +1,24 @@
+import numpy as np
+inp= np.array(list(map(int, input("Enter you marks WITH SP:").split())))
+print(inp)
+print(np.percentile(inp, 50))
+
+
+# EXPLANATION:
+# input()
+# Takes input as one string.
+
+# .split()
+# Splits the string by spaces.
+
+# map(int, ...)
+# map() applies a function to every element.
+# Here it converts each string → integer.
+
+# list(...)
+# Converts the map object into a list.
+
+# Then NumPy converts it to array
+
+
+# take numbers from user → convert to integers → store in list
